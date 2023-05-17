0dd458984842d6c4d31899a00cd154f2c9663b42
Aali7575 <tafadzwaaaliyahndoro@gmail.com>
2023-05-16 22:37:12 -0700

    still working on question 1

--- first.c
+++ first.c
@@ -8,17 +8,40 @@ int main(int argc, char **argv)
 	ssize_t written;
 	size_t len;
 	(void)STDOUT_FILENO;
+	char *buffer = NULL; /*this is for our getline function*/
+	size_t bufsize;
+	ssize_t gettext;
+	ssize_t console;
+	size_t length;
+	const char *delim; /*this is for tokenization*/
+	char *bufcopy;
 
-	prompt = ("Simple_shell $");
+	/*prompt for our shell*/
+	prompt = ("Simple_shell $ ");
 	len = strlen(prompt);
 		
-
+/*enabling it to print to the console*/
+	while(1)
+	{
 	written = write(STDOUT_FILENO, prompt, len);
 
 	if (written == -1)
 	{
 		return (1);
 	}
+	gettext = getline(&buffer,&bufsize,stdin);
+
+	if (gettext == -1)
+	{
+		errno = EINVAL;
+		perror("Exiting shell");
+		return (-1);
+	}
+
+	length = strlen(buffer);
+	console = write(STDOUT_FILENO,buffer,length);
+	}
+	free(buffer);
 	return (0);
 }
 
--- main.h
+++ main.h
@@ -4,6 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+
+
 
 
 #endif
