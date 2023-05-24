500dafecfe0148fbd29bc93d1ad3fd15277de54f
Aali7575 <tafadzwaaaliyahndoro@gmail.com>
2023-05-24 06:19:33 -0700

    tried fixing errors from our main file

--- practise.c
+++ practise.c
@@ -1,90 +1,86 @@
 #include "main.h"
 
+/**
+ * exem -  our file for execution - this is our main file
+ * @argv: argument vector
+ */
+
 void exem(char **argv)
 {
-        char *pname = NULL;
+	char *pname = NULL;
 
-        if(argv)
-        {
-                pname = argv[0];
+	if (argv)
+	{
+		pname = argv[0];
 
-
-if(execve(pname,argv,NULL) == -1)
-{
-        perror("Error");
+		if (execve(pname, argv, NULL) == -1)
+		{
+			perror("Error");
+		}
+	}
 }
-
-}
-
-}
+/**
+ * main - our main file
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 1 when successful -1 when unsuccessful
+ */
 
 int main(int argc, char **argv)
 {
 	(void)argc;
 	char *prompt;
 	size_t prompt_len;
-	ssize_t prompt_mess;
-	ssize_t prompt_get;
+	ssize_t prompt_mess, prompt_get;
 	size_t line_size = 0;
-	char *lineptr = NULL;
-	char *token;
-	char *token2;
+	char *lineptr = NULL, *token, *token2;
 	const char *delim = (" \n");
 	char *lineptr_copy;
-	int tokens_number;
-	int i;
+	int tokens_number, i;
 
-	while(1)
+	while (1)
 	{
-	prompt = ("$ ");
-	prompt_len = strlen(prompt);
-	prompt_mess = write(STDOUT_FILENO,prompt,prompt_len);
+		prompt = ("$ ");
+		prompt_len = strlen(prompt);
+		prompt_mess = write(STDOUT_FILENO, prompt, prompt_len);
+		prompt_get = getline(&lineptr, &line_size, stdin);
 
-	prompt_get = getline(&lineptr,&line_size,stdin);
+		if (prompt_get == -1)
+		{
+			perror("Exiting shell..");
+			return (-1);
+		}
+		lineptr_copy = strdup(lineptr);
 
-	if (prompt_get == -1)
-	{
-		perror("Exiting shell..");
-		return (-1);
+		if (lineptr_copy == NULL)
+		{
+			perror("memory allocation error");
+			return (-1);
+		}
+		token = strtok(lineptr, delim);
+
+		while (token != NULL)
+		{
+			tokens_number++;
+			token = strtok(NULL, delim);
+		}
+		tokens_number++;
+		token2 = strtok(lineptr_copy, delim);
+		argv = malloc(sizeof(char *) * tokens_number);
+
+		for (i = 0; token2 != NULL; i++)
+		{
+			argv[i] = malloc(sizeof(char) * strlen(token2));
+			strcpy(argv[i], token2);
+			token2 = strtok(NULL, delim);
+		}
+		argv[i] = NULL;
+		free(argv[i]);
+		exem(argv);
 	}
-
-	lineptr_copy = strdup(lineptr);
-
-	if (lineptr_copy == NULL)
-	{
-		perror("memory allocation error");
-		return (-1);
-	}
-	token = strtok(lineptr, delim);
-
-	 while(token != NULL)
-	 {
-		 tokens_number++;
-			 token = strtok(NULL, delim);
-	 }
-
-	 tokens_number++;
-	 token2 = strtok(lineptr_copy, delim);
-
-	 argv = malloc(sizeof(char *) * tokens_number);
-
-	 for (i=0; token2 != NULL; i++)
-	 {
-		 argv[i] = malloc(sizeof(char) * strlen(token2));
-				 strcpy(argv[i], token2);
-				 token2 = strtok(NULL, delim);
-				 }
-				 argv[i] = NULL;
-				 free(argv[i]);
-				 exem(argv);
-
-
-	}
-
 	free(lineptr);
 	free(lineptr_copy);
 	free(argv);
 
 	return (0);
-
 }
